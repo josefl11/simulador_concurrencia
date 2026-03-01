991c3612ebe369cf8d0236e74c16a62219914f6b
josefl11 <jose.florian472@pascualbravo.edu.co>
2026-03-01 15:08:32 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,31 @@
+import threading
+import time
 
+documento = ""  # documento compartido
+lock = threading.Lock()  # bloquea para que un hilo escriba a la vez
+
+def usuario(nombre, texto):
+    global documento
+    for letra in texto:
+        with lock:  # asegura que solo un usuario escriba en este momento
+            documento += letra
+            print(f"{nombre} escribió: {letra}")
+        time.sleep(0.2)
+    print(f"{nombre} terminó de escribir.\n")
+
+# hilos para cada usuario
+personaA = threading.Thread(target=usuario, args=("Sebastian", "Los beneficios de jugar básquet"))
+personaB = threading.Thread(target=usuario, args=("Carla", "Amo los gatos porque son tiernos"))
+personaC = threading.Thread(target=usuario, args=("Stiven", "Hola a todas las personas del mundo"))
+
+# iniciamos los hilos
+personaA.start()
+personaB.start()
+personaC.start()
+
+# esperamos a que terminen
+personaA.join()
+personaB.join()
+personaC.join()
+
+print("\nDocumento final:", documento)
